d89d7a2bdd8b01c76b1bf0ead9b1339340231d02
alaa <mejdi.alaa@gmail.com>
2022-12-26 00:28:49 +0200

    complet

--- src/main/java/de/tekup/studentsabsence/services/EmailService.java
+++
@@ -1,7 +0,0 @@
-package de.tekup.studentsabsence.services;
-
-import de.tekup.studentsabsence.entities.Student;
-import de.tekup.studentsabsence.entities.Subject;
-
-public interface EmailService {
-    void sendEEmail(Student student, Subject subject);
---
+++ src/main/java/de/tekup/studentsabsence/services/impl/EmailService.java
@@ -0,0 +1,8 @@
+paquet  de . tekup . absence des étudiants . services ;
+
+        importer  de . tekup . absence des étudiants . entités . Étudiant ;
+        importer  de . tekup . absence des étudiants . entités . Sujet ;
+
+interface  publique EmailService {
+    void  sendEEmail ( Étudiant  étudiant , Sujet  sujet );
+}

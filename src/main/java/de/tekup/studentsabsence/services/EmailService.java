90b2316cdaff17fbd984da26ecd3a7e69b724dc3
alaa <mejdi.alaa@gmail.com>
2022-12-26 00:10:28 +0200

    complet

---
+++ src/main/java/de/tekup/studentsabsence/services/EmailService.java
@@ -0,0 +1,7 @@
+package de.tekup.studentsabsence.services;
+
+import de.tekup.studentsabsence.entities.Student;
+import de.tekup.studentsabsence.entities.Subject;
+
+public interface EmailService {
+    void sendEEmail(Student student, Subject subject);
